8fdedcace558d1dae6ac74ec401addef15e4f2b8
Christian Gallitzendörfer <christian.gallitzendoerfer@dcs-fuerth.de>
2016-03-15 15:57:37 +0100

    first commit

---
+++ app/components/Profile.js
@@ -0,0 +1,51 @@
+var React = require('react');
+var Router = require('react-router');
+var Repos = require('./github/Repos');
+var UserProfile = require('./github/UserProfile');
+var Notes = require('./notes/Notes');
+var ReactFireMixin = require('reactfire');
+var Firebase = require('firebase');
+
+var Profile = React.createClass({
+  mixins: [ReactFireMixin],
+  getInitialState: function() {
+    return {
+      repos: [],
+      bio: {
+        name: ''
+      },
+      notes: []
+    }
+  },
+  componentDidMount: function() {
+    this.ref = new Firebase('https://burning-torch-877.firebaseio.com/');
+    var childRef = this.ref.child(this.props.params.username);
+    this.bindAsArray(childRef, 'notes');
+  },
+  componentWillUnmount: function() {
+    this.unbind('notes');
+  },
+  handleAddNote: function(newNote) {
+    this.ref.child(this.props.params.username).child(this.state.notes.length).set(newNote);
+  },
+  render: function() {
+    return (
+      <div className="row">
+        <div className="col-md-4">
+          <UserProfile username={this.props.params.username} bio={this.state.bio} />
+        </div>
+        <div className="col-md-4">
+          <Repos username={this.props.params.username} repos={this.state.repos} />
+        </div>
+        <div className="col-md-4">
+          <Notes
+            addNote={this.handleAddNote}
+            username={this.props.params.username}
+            notes={this.state.notes} />
+        </div>
+      </div>
+    )
+  }
+});
+
+module.exports = Profile;
